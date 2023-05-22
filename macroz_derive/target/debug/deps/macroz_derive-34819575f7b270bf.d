8c6bd589511c6cb10cd48b93fb910c1540ca2db0
Austin Rhodes <warhodes@gmail.com>
2023-05-21 23:44:03 -0400

    initial

--- macroz_derive/target/debug/deps/macroz_derive-34819575f7b270bf.d
+++
@@ -1,5 +0,0 @@
-/home/warhodes/rust/macroz/macroz_derive/target/debug/deps/macroz_derive-34819575f7b270bf.rmeta: src/lib.rs
-
-/home/warhodes/rust/macroz/macroz_derive/target/debug/deps/macroz_derive-34819575f7b270bf.d: src/lib.rs
-
-src/lib.rs:
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
-pub trait TypeName {
-    fn print_typename();
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_attribute]
+pub fn cached(
+    _attr: TokenStream,
+    item: TokenStream,
+) -> TokenStream {
+    let ast: syn::ItemFn = syn::parse(item)
+        .expect("Failed to parse TokenStream. Check that the return type is correct.");
+    impl_prepend_task(&ast)
 }
 
-/*
-impl TypeName for Pancakes {
-    fn print_typename() {
-        println!("My typename is Pancakes!")
-    }
+fn impl_prepend_task(ast: &syn::ItemFn) -> TokenStream {
+    let func_name = &ast.sig.ident;
+    let func_body = &ast.block;
+    let gen = quote! {
+        fn #func_name() {
+            println!("This occurs before the function is run");
+            #func_body
+            println!("This occurs after the function is run");
+        }
+    };
+    gen.into()
 }
-*/
