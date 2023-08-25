d2b0b2df8d5e4d6d9ccddecf6a1a0c3feffb6793
warhodes-dev <warhodes@gmail.com>
2023-08-24 21:35:29 -0400

    commiting final changes and LRU implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,133 @@
+#![feature(let_chains)]
+
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::format_ident;
-use syn::{self, spanned::Spanned};
+use quote::{format_ident, quote};
+use syn::{self, parse_quote};
+use darling::FromMeta;
+use darling::ast::NestedMeta;
+
+#[derive(Debug, FromMeta)]
+struct MacroArgs {
+    #[darling(default)]
+    size: Option<usize>,
+}
+
+#[derive(Debug)]
+enum CacheType {
+    LruCache,
+    HashMap,
+}
+
+#[derive(Debug)]
+struct Config {
+    size: Option<usize>,
+    cache_type: CacheType,
+}
+
+impl Config {
+    fn from(args: &MacroArgs) -> Result<Self, Box<dyn std::error::Error>> {
+        let cache_type = match args.size {
+            Some(0) => return Err("Cannot create cache of size 0.".into()),
+            Some(_) => CacheType::LruCache,
+            None => CacheType::HashMap,
+        };
+        Ok( Config { 
+            size: args.size, 
+            cache_type 
+        })
+    }
+}
 
 #[proc_macro_attribute]
-pub fn cached(
-    _attr: TokenStream,
-    item: TokenStream,
-) -> TokenStream {
+pub fn cached(args: TokenStream, item: TokenStream) -> TokenStream {
     let ast: syn::ItemFn = syn::parse(item)
         .expect("Failed to parse TokenStream. Check that the return type is correct.");
 
-    impl_cached(&ast)
+    let macro_args_list = NestedMeta::parse_meta_list(args.into())
+        .expect("Failed to parse macro arguments");
+
+    let macro_args = MacroArgs::from_list(&macro_args_list)
+        .expect("Failed to build darling structure from macro arguments");
+
+    let config = Config::from(&macro_args)
+        .expect("Failed to build config from macro arguments");
+
+    if cfg!(debug_assertions) {
+        println!("Cache Config:\n{config:#?}");
+    }
+
+    impl_cached(&ast, &config)
 }
 
-fn impl_cached(func: &syn::ItemFn) -> TokenStream {
-    let fn_name = &func.sig.ident;
-    let fn_sig = &func.sig;
-    let fn_body = &func.block;
+fn impl_cached(func: &syn::ItemFn, config: &Config) -> TokenStream {
+    let (param_ids, param_types): (Vec<_>, Vec<_>) = func.sig.inputs.iter()
+        .map(|argument| {
+            if let syn::FnArg::Typed(parameter) = argument
+            && let syn::Pat::Ident(parameter_id) = &*parameter.pat {
+                let param_type = &*parameter.ty;
+                let param_id = &parameter_id.ident;
+                (param_id, param_type)
+            } else { 
+                unimplemented!("Unsupported argument form."); 
+            }
+        }).unzip();
 
-    let memo_fn_name = quote::format_ident!("memoized_{}", fn_name);
-    let flush_name = syn::Ident::new(&format!("memoized_flush_{}", fn_name), fn_sig.span());
-    let map_name = format_ident!("memoized_mapping_{}", fn_name);
+    let key_type: syn::Type = parse_quote!((#(#param_types),*));
 
-    let inputs = get_input_pairs(&func.sig.inputs);
-
-    let gen = quote::quote! {
-        #fn_sig {
-            println!("This occurs before the function is run");
-            #fn_body
-            // Be careful inserting code here. Early implicit returns might get suppressed.
-            // Perhaps you can replace early implicit returns with explicit returns?
-        }
+    let syn::ReturnType::Type(_, value_type) = &func.sig.output 
+    else {
+        panic!("Should not memoize function with no return type");
     };
-    gen.into()
-}
 
-fn get_input_pairs(
-    inputs: &syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma>
-) -> Vec<(syn::Ident, syn::Type)> {
-    inputs
-        .iter()
-        .map(|input| match input {
-            syn::FnArg::Receiver(_) => panic!("methods (functions taking 'self') are not supported"),
-            syn::FnArg::Typed(pat_type) => (*match_pattern_type(&pat_type),*pat_type.ty.clone()),
-        })
-        .collect()
-}
+    let func_sig = &func.sig;
+    let func_body = &func.block;
+    let cache_id = format_ident!("{}_CACHE", &func.sig.ident.to_string().to_uppercase());
 
-fn match_pattern_type(pat_type: &&syn::PatType) -> Box<syn::Ident> {
-    match &std::ops::Deref::deref(&pat_type.pat) {
-        syn::Pat::Ident(pat_ident) => {
-            if pat_ident.mutability.is_some() {
-                let mut pat_ident_is_mut = pat_ident.clone();
-                pat_ident_is_mut.mutability = None;
-                Box::new(pat_ident_is_mut.ident.clone())
+    let (cache_type, cache_init, cache_insert_method) = match config.cache_type {
+        CacheType::LruCache => {
+            if let Some(size) = config.size {
+                (  
+                    quote! { lru::LruCache<#key_type, #value_type> },
+                    quote! { lru::LruCache::new(std::num::NonZeroUsize::new(#size).unwrap()) },
+                    quote! { put },
+
+                )
             } else {
-                Box::new(pat_ident.ident.clone())
+                panic!("Can't have LRU Cache of size 0")
+            }
+        },
+        CacheType::HashMap => {
+            (
+                quote! { std::collections::HashMap<#key_type, #value_type> },
+                quote! { std::collections::HashMap::new() },
+                quote! { insert },
+            )
+        }
+    }; 
+
+    let memoized_func = quote! {
+        thread_local! {
+            static #cache_id: std::cell::RefCell<#cache_type> =
+                std::cell::RefCell::new(#cache_init)
+        }
+
+        #func_sig {
+            // Note, nightly <cell>.with_borrow_mut() cannot be used *or enabled* here because we are 
+            // in the macro expansion. The end user would have to enable this with #![feature(...)]
+            let key = (#(#param_ids),*);
+            if let Some(value) = #cache_id.with(|cache| cache.borrow_mut().get(&key).cloned() ) {
+                value
+            } else {
+                let value = (||#func_body)();
+                #cache_id.with(|cache| {
+                    cache.borrow_mut().#cache_insert_method(key, value.clone());
+                });
+                value
             }
         }
-        _ => panic!("Patterns as input args not supported")
-    }
+    };
+
+    memoized_func.into()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,23 @@ use cache_monet::cached;
 
 use std::env;
 
-#[cached]
-fn fib(n: u128) -> u128 {
+#[cached(size = 50)]
+fn fib(n: u128) -> u128{
     if n <= 1 { n } 
     else { fib(n - 1) + fib(n - 2) }
 }
 
 fn main() {
-    let n;
-
     let mut args = env::args();
 
-    if args.len() != 2 {
-        eprintln!("usage: fib <number>");
-        std::process::exit(1);
-    } else {
-        n = args.nth(1)
-            .unwrap()
-            .parse::<u128>()
-            .expect("Could not parse input as integer");
-    }
+    let n = args.nth(1)
+        .unwrap_or_else(||{
+            eprintln!("usage: fib <number>");
+            std::process::exit(1);
+        })
+        .parse::<u128>()
+        .expect("Failed to parse argument");
 
-    let answer = fib(n);
-
-    println!("{answer}")
+    let result = fib(n);
+    println!("{result}")
 }
